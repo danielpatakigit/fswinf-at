e7c63ae68e118a322dc57c18ae3b4a896072b53e
Dániel Pataki <e12223375@student.tuwien.ac.at>
2023-12-11 00:26:26 +0100

    feat: work on landing page

--- www/src/routes/[lang]/+layout.svelte
+++ www/src/routes/[lang]/+layout.svelte
@@ -3,9 +3,6 @@
 	import { page } from "$app/stores";
 	import { goto } from "$app/navigation";
 	import { writable } from "svelte/store";
-
-	import "../../app.pcss";
-
 	import {
 		ModeWatcher,
 		toggleMode,
@@ -13,22 +10,23 @@
 		setMode,
 		resetMode,
 	} from "mode-watcher";
-
 	import Icon from "@iconify/svelte";
 	import { fade } from "svelte/transition";
-
 	import { onNavigate } from "$app/navigation";
+	import "../../app.pcss";
+	import logo from "/src/static/logo_Marginless.png";
+	import { Hamburger } from "svelte-hamburgers";
 
-	onNavigate((navigation) => {
-		if (!document.startViewTransition) return;
+	// onNavigate((navigation) => {
+	// 	if (!document.startViewTransition) return;
 
-		return new Promise((resolve) => {
-			document.startViewTransition(async () => {
-				resolve();
-				await navigation.complete;
-			});
-		});
-	});
+	// 	return new Promise((resolve) => {
+	// 		document.startViewTransition(async () => {
+	// 			resolve();
+	// 			await navigation.complete;
+	// 		});
+	// 	});
+	// });
 
 	export let data;
 	let filteredPosts = [];
@@ -41,14 +39,112 @@
 	const count = writable(2);
 
 	$: ({ route } = $page.data);
+
+	let open;
+	let search = false;
+	function toggleSearch() {
+		search = !search;
+	}
 </script>
 
 <ModeWatcher defaultMode={"dark"}></ModeWatcher>
 
-<a href="/{$locale}">{$t("menu.home")}</a>
+<header class="min-h-[3rem] relative flex justify-between">
+	<div class="flex gap-2 flex-row items-center justify-center w-52">
+		<Icon icon="mdi:home"></Icon>
+		<Icon icon="mdi:home"></Icon>
+		<Icon icon="mdi:home"></Icon>
+		<Icon icon="mdi:home"></Icon>
+	</div>
+	<img
+		class="max-h-12 m-auto"
+		src={logo}
+		alt="winf-logo"
+	/>
+	<div class="flex items-center justify-center gap-2 w-52">
+		<select on:change={({ target }) => goto(target.value)}>
+			{#each $locales as lc}
+				<option
+					value="/{lc}{route}"
+					selected={lc === $locale}
+				>
+					{lc}
+				</option>
+			{/each}
+		</select>
+		<button
+			on:click={toggleMode}
+			class=""
+		>
+			<Icon
+				icon={"mdi:theme-light-dark"}
+				class="text-4xl"
+			></Icon>
+		</button>
+		<button on:click={toggleSearch}>
+			<Icon
+				icon={"mdi:search"}
+				class="text-4xl"
+			></Icon>
+		</button>
+		<button>
+			<Hamburger bind:open></Hamburger>
+		</button>
+	</div>
+	<nav
+		class=" {open ? 'visible' : 'invisible'}
+		absolute bg-white dark:bg-black grid grid-cols-3 gap-2 border p-4 h-[90vh] mx-auto sm:h-auto top-[100%] w-screen max-w-full"
+	>
+		{#each Array(8) as _, i}
+			<a
+				href="##"
+				class=" flex gap-4 border p-2 relative group/tooltip hover:cursor-pointer"
+			>
+				<div
+					class="flex items-center justify-center aspect-squares w-24"
+				>
+					<Icon
+						icon="mdi:home"
+						class="text-4xl"
+					></Icon>
+				</div>
+				<div class="flex flex-col gap-1">
+					<span
+						href="##"
+						class="font-bold"
+					>
+						Lorem ipsum dolor sit amet.
+					</span>
+				</div>
+				<span
+					class="font-light absolute z-[10] bg-black text-white text-center py-2 px-4 rounded-md opacity-0 group-hover/tooltip:opacity-100 transition-opacity invisible group-hover/tooltip:visible top-[110%]"
+				>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				</span>
+			</a>
+		{/each}
+		<div></div>
+	</nav>
+</header>
+
+<div
+	id="modal"
+	class="{search
+		? 'visible'
+		: 'invisible'} fixed h-screen w-screen max-w-full flex items-center justify-center backdrop-blur-sm bg-white/30"
+>
+	<div class="border">
+		<h2>Search for anything</h2>
+		<div class="flex flex-row">
+			<Icon icon="mdi:search"></Icon>
+			<input type="text" />
+		</div>
+	</div>
+</div>
+
+<!-- <a href="/{$locale}">{$t("menu.home")}</a>
 <a href="/{$locale}/about">{$t("menu.about")}</a>
 <hr />
-<br />
 <h2>Links to posts</h2>
 <ul>
 	{#each filteredPosts as post}
@@ -60,10 +156,7 @@
 		</li>
 	{/each}
 </ul>
-
 <hr />
-<br />
-<br />
 {$t("menu.notification", { count: $count })}
 <br />
 <button
@@ -79,31 +172,7 @@
 	}}
 >
 	+
-</button>
-<hr />
-<!-- {#key data.url} -->
-<!-- <main transition:fade> -->
-<slot />
-<!-- </main> -->
-<!-- {/key} -->
-
-<br />
-<select on:change={({ target }) => goto(target.value)}>
-	{#each $locales as lc}
-		<option
-			value="/{lc}{route}"
-			selected={lc === $locale}
-		>
-			{$t(`lang.${lc}`)}
-		</option>
-	{/each}
-</select>
-<hr />
-<br />
-<button on:click={toggleMode}>
-	<Icon
-		icon={"mdi:theme-light-dark"}
-		class="text-4xl"
-	></Icon>
-	<span>{$mode}</span>
-</button>
+</button> -->
+<main>
+	<slot />
+</main>
--- www/src/routes/[lang]/+page.svelte
+++ www/src/routes/[lang]/+page.svelte
@@ -1,8 +1,36 @@
 <script>
-	import { t } from "$lib/translations";
-
-	const link = "https://kit.svelte.dev";
+	import Icon from "@iconify/svelte";
 </script>
 
-<h1>{$t("home.title")}</h1>
-<p>{@html $t("home.text", { link })}</p>
+<section class="border text-center pt-24 min-h-[40rem] flex flex-col">
+	<div class="">
+		<div>
+			<h1 class="text-3xl font-bold">
+				Fachschaft Wirtschaftsinformatik und Data Science
+			</h1>
+			<h2>
+				und Studienvertretung für Wirtschaftsinformatik und Data
+				Science an der TU Wien
+			</h2>
+		</div>
+		<div></div>
+	</div>
+</section>
+<section
+	id="quick-links"
+	class="bg-blue-200"
+>
+	<article class="">
+		<h2 class="font-bold text-xl">Quick Links</h2>
+		<div class="grid grid-cols-4">
+			{#each Array(4) as _, i}
+				<div
+					class="border p-4 flex justify-between items-center bg-white"
+				>
+					<span>Lorem</span>
+					<Icon icon="mdi:arrow"></Icon>
+				</div>
+			{/each}
+		</div>
+	</article>
+</section>
